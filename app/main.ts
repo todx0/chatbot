8850d7f3e7eda6e84f1991e3c08ce6174c94f91d
= <=>
2023-05-30 14:19:20 +0200

    test

--- app/main.ts
+++ app/main.ts
@@ -271,7 +271,7 @@ const processCommand = async (event: any) => {
 			const currentHistory = await getHistory(replHistory);
 			const replyTo = message.originalArgs.message;
 			// const gptRequest = `This message is yours: ${messageResponse}. \n This was a person's reply to it: ${replyTo}. \n Reply to person's message in his language in a little of sarcastic way and sound that you are annoyed. Reply without 'User:','You:' and 'Person:'. Whole conversation: ${currentHistory}`;
-			const gptRequest = `This is conversation history: ${currentHistory} \n This is your message: ${messageResponse} \n Here is a person's reply to your message: ${replyTo}. Only reply to person's message in his language in sarcastic tone. Do not include 'User:','You:' and 'Person:' in reply.`;
+			const gptRequest = `This is the conversation history: ${currentHistory} \n This is your message: ${messageResponse} \n Here is a person's reply to your message: ${replyTo}. When replying to the person's message in a sarcastic tone, use their language and avoid including 'User:', 'You:', and 'Person:' in your reply`;
 			const gptReply = await generateGptResponse(gptRequest);
 			await replyToMessage(gptReply, message.id, groupId);
 			await writeToHistoryFile(`User: ${replyTo}`, replHistory);
