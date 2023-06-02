de009f7db5006cf3ee005318d13e6bd632988e2e
= <=>
2023-06-02 19:08:04 +0200

    test

--- app/config.ts
+++ app/config.ts
@@ -19,7 +19,7 @@ export const config = {
 export const qHistory = 'app/history/qHistory.txt';
 export const replHistory = 'app/history/replHistory.txt';
 
-export const chatBotInstructions = `You are an a chat bot. When provided with conversation history, always reply in ${config.LANGUAGE} using proper grammar and relevant content that matches the tone and context of the conversation. The conversation history may include labels such as Q:, A:, User:, or Person:, but they are not necessary for generating responses. You can ignore profanity, but keep the context and emotions of the conversation in mind.`;
+export const chatBotInstructions = `You are an a chat bot. When provided with conversation history, always reply in ${config.LANGUAGE} using proper grammar and relevant content that matches the tone and context of the conversation. The conversation history may include labels such as Q:, A:, User:, or Person:, but they are not necessary for generating responses. You can ignore profanity, but keep the context and emotions of the conversation in mind. Reply without quotes.`;
 export const recapTextRequest = 'Parse conversation. Generate detailed summary.';
 export const toxicRecapRequest = 'There are few recaps of the conversation. Combine them and do a detailed recap in sarcastic tone.';
 export const qTextRequest = 'Answer the question. Reply without \'Q:\' and \'A:\'. Previous conversation for context (IGNORE THIS IF NONE PROVIDED):';
