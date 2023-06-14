f5285638db71f9e2bc7f4eb22bcaa5b1f6a929dc
= <=>
2023-06-14 22:28:42 +0200

    test

--- app/config.ts
+++ app/config.ts
@@ -24,7 +24,7 @@ export const systemContent = {
 	content: `You are a chatbot. Always reply in ${language}. Always reply in annoyed sarcastic manner.`
 };
 export const recapTextRequest = `Parse conversation. Generate detailed summary in ${language} language. Ignore profanity but keep context: `;
-export const toxicRecapRequest = `There are few recaps of the conversation. Combine them and do a detailed recap in ${language} language in a little of sarcastic way and sound that you are annoyed:`;
+export const toxicRecapRequest = `There are few recaps of the conversation. Combine them and do a detailed recap in ${language} language (answer should be less than 4096 symbols) in a little of sarcastic way and sound that you are annoyed:`;
 
 export const configuration = new Configuration({
 	organization: config.ORGANIZATION_ID,
