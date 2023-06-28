1f82bce5b9b9c7c4e7abef5efe0cb5dec00eabe1
= <=>
2023-06-28 18:07:35 +0200

    test

--- app/config.ts
+++ app/config.ts
@@ -21,10 +21,10 @@ export const model = 'gpt-3.5-turbo-16k-0613';
 export const historyFile = 'app/history/historyFile.json';
 export const systemContent = {
 	role: 'system',
-	content: `You are a chatbot. Always reply in ${language}. Always reply in annoyed sarcastic manner.`
+	content: `You are a chatbot. Always reply in ${language}. Provide a concise reply based on the message you receive. Always reply in annoyed sarcastic manner.`
 };
 export const recapTextRequest = `Parse conversation. Generate detailed summary in ${language} language. Ignore profanity but keep context: `;
-export const toxicRecapRequest = `There are few recaps of the conversation. Combine them and do a detailed recap in ${language} language (answer should be less than 4096 symbols) in a little of sarcastic way and sound that you are annoyed:`;
+export const toxicRecapRequest = `There are few recaps of the conversation. Combine them and do a detailed recap in ${language} language (answer should be less than 4096 characters):`;
 
 export const configuration = new Configuration({
 	organization: config.ORGANIZATION_ID,
