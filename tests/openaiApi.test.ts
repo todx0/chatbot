ea4dfaf6314452657ea1a67ee50c0f50c4ad6f1e
= <=>
2023-09-15 14:25:01 +0200

    fix

--- tests/openaiApi.test.ts
+++ tests/openaiApi.test.ts
@@ -13,9 +13,10 @@ import {
 
 describe('main functions', () => {
 	const mockResponseContent = 'Mock response content';
-	const mockImage = 'https://oaidalleapiprodscus.blob.core.windows.net/private/org-25EUekyHinNOumFccWsaSMNp/user-FX1HxrEvbPTBhpULVmMuNoAv/img-mXItIHH2lMLXZnbDYY6LPHA6.png?st=2023-09-15T10%3A40%3A26Z&se=2023-09-15T12%3A40%3A26Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-09-15T06%3A16%3A55Z&ske=2023-09-16T06%3A16%3A55Z&sks=b&skv=2021-08-06&sig=m6n0ljyg8d72Sj5rPM2UvXcR90JGn2OuTTD92KwzGbg%3D';
+	const mockImage = 'https://oaidalleapiprodscus.blob.core.windows.net/private/org-TEST/user-TEST/img-TEST.png';
+
 	beforeEach(async () => {
-		const headers = JSON.parse('{"Accept":"application/json, text/plain, */*","Content-Type":"application/json","User-Agent":"OpenAI/NodeJS/3.3.0","Authorization":"Bearer sk-test","OpenAI-Organization":"TEST","Content-Length":68}');
+		const headers = JSON.parse('{"Accept":"application/json, text/plain, */*","Content-Type":"application/json","User-Agent":"OpenAI/NodeJS/3.3.0","Authorization":"Bearer sk-test","OpenAI-Organization":"TEST","Content-Length":69}');
 		const mockChatCompletionResponse: AxiosResponse<CreateChatCompletionResponse, any> = {
 			data: {
 				id: 'testId',
