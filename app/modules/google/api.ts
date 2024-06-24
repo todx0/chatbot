ee2e2371a250fb97cde1039554dce20e588efba9
todx0 <todxsatan@gmail.com>
2024-06-24 16:52:15 +0200

    fix retry

--- app/modules/google/api.ts
+++ app/modules/google/api.ts
@@ -1,9 +1,9 @@
 import { Content } from '@google/generative-ai';
-import { config, featureFlags, genAImodel, maxHistoryLength, recapTextRequest, safetySettings } from '../../config';
+import { config, genAImodel, maxHistoryLength, recapTextRequest, safetySettings } from '../../config';
 import { ErrorHandler } from '../../errors/ErrorHandler';
-import { insertToMessages, readChatRoleFromDatabase } from '../../helper';
+import { insertToMessages, readChatRoleFromDatabase, retry } from '../../helper';
 
-export async function generateGenAIResponse(userRequest: string): Promise<string> {
+/* export async function generateGenAIResponse(userRequest: string): Promise<string> {
   try {
     const userRoleContent: Content = { role: 'user', parts: [{ text: userRequest }] };
     const history: Content[] = await readChatRoleFromDatabase({ limit: maxHistoryLength });
@@ -23,6 +23,56 @@ export async function generateGenAIResponse(userRequest: string): Promise<string
     await insertToMessages(userRoleContent);
     await insertToMessages({ role: 'model', parts: [{ text: responseText }] });
 
+    return responseText;
+  } catch (error: any) {
+    return ErrorHandler.handleError(error);
+  }
+} */
+
+export async function generateGenAIResponse(userRequest: string): Promise<string> {
+  let retryCount = 0;
+  let retryRequestDisclaimer = [
+    ``,
+    `Provide a concise counterpoint to the message's viewpoint:`,
+    `Offer a surprising twist on the message's underlying theme:`,
+    `Generate a brief thought experiment related to the message's concept:`,
+    `Offer an alternative perspective on the topic of the message:`,
+  ];
+  const disclaimer = `This message is for informational purposes only and does not promote violence,
+  hate speech, or illegal activities.`;
+  async function fetchGenAIResponse(): Promise<string> {
+    try {
+      const userRoleContent: Content = { role: 'user', parts: [{ text: userRequest }] };
+      const history: Content[] = await readChatRoleFromDatabase({ limit: maxHistoryLength });
+
+      const chat = genAImodel.startChat({
+        history,
+        safetySettings,
+      });
+
+      const request = !retryCount
+        ? userRequest
+        : `${disclaimer} ${retryRequestDisclaimer[retryCount]} ${userRequest} \n Reply in ${config.LANGUAGE}`;
+
+      retryCount += 1;
+      const result = await chat.sendMessage(request);
+
+      const { response } = result;
+      let responseText = response.text();
+
+      if (!responseText) responseText = 'Бля я хуй знает дядя.';
+
+      await insertToMessages(userRoleContent);
+      await insertToMessages({ role: 'model', parts: [{ text: responseText }] });
+
+      return responseText;
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
+
+  try {
+    const responseText = await retry(fetchGenAIResponse, retryRequestDisclaimer.length);
     return responseText;
   } catch (error: any) {
     return ErrorHandler.handleError(error);
