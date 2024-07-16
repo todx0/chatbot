f4b83f159a0cfb85dc62822678b3a68910e1dd6d
todx0 <todxsatan@gmail.com>
2024-07-16 17:37:07 +0200

    cleanup

--- app/main.ts
+++ app/main.ts
@@ -1,14 +1,7 @@
 import { Api } from 'telegram';
 import TelegramBot from './bot';
 import { initConfig, telegramClient } from './config';
-import {
-  createMessagesTable,
-  getDataFromEvent,
-  isRandomReply,
-  messageNotSeen,
-  shouldTranscribeMedia,
-  somebodyMentioned,
-} from './utils/helper';
+import { createMessagesTable, isRandomReply, shouldTranscribeMedia, somebodyMentioned } from './utils/helper';
 
 const bot = new TelegramBot(telegramClient);
 
